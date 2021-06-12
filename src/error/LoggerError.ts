b8f6cf73aaafb69d3f3307ee368d2ef1eba95b07
Jonn Nilsson <jonn@jonn.nu>
2021-06-12 09:10:08 +0200

    fix: extend error from lindorm error

--- src/error/LoggerError.ts
+++ src/error/LoggerError.ts
@@ -1,7 +1,3 @@
-import { ExtendableError, ExtendableErrorOptions } from "@lindorm-io/errors";
+import { LindormError } from "@lindorm-io/errors";
 
-export class LoggerError extends ExtendableError {
-  public constructor(message: string, options?: ExtendableErrorOptions) {
-    super(message, options);
-  }
-}
+export class LoggerError extends LindormError {}
